fec2ea1773120bb3161766bf7487bdebcd883932
ultramunnn <munir63577@gmail.com>
2024-11-04 23:39:25 +0700

    2

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
 
 menuOpenButton.addEventListener("click", () => {
   document.body.classList.toggle("show-mobile-menu");
 });
+
+menuCloseButton.addEventListener("click", () => menuOpenButton.click());
